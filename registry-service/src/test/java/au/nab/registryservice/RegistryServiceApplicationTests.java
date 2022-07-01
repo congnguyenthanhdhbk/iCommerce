e039cfbded4533fbde72b2ff6c9cf6e19cbd86d7
cong.nguyen <cong.nguyenthanh.2304@gmail.com>
2022-07-01 22:46:34 +0700

    Add registry service

---
+++ registry-service/src/test/java/au/nab/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package au.nab.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
